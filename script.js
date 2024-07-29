ad34bcaa954df747b4c7b64de9a8b78c4ff3304b
rafaelvicenzo <rafaellordes.lordes@gmail.com>
2024-07-29 20:14:26 -0300

    Search implementation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
     const apiURL = 'https://homologacao3.azapfy.com.br/api/ps/metahumans';
@@ -6,18 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentPage = 1;
     let totalPages = 1;
     let data = [];
+    let filteredData = [];
     let selectedHeroes = [];
 
     async function fetchMetahumanData() {
         try {
             const response = await fetch(apiURL);
             data = await response.json();
+            filteredData = data;
 
             if (!data || data.length === 0) {
                 throw new Error('Nenhum dado encontrado na API');
             }
 
-            totalPages = Math.ceil(data.length / itemsPerPage);
+            totalPages = Math.ceil(filteredData.length / itemsPerPage);
             displayPage(currentPage);
             createPagination();
         } catch (error) {
@@ -32,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const start = (page - 1) * itemsPerPage;
         const end = start + itemsPerPage;
-        const pageItems = data.slice(start, end);
+        const pageItems = filteredData.slice(start, end);
 
         pageItems.forEach(metahuman => {
             const card = document.createElement('div');
@@ -55,22 +57,29 @@ document.addEventListener('DOMContentLoaded', function () {
                 </div>
             `;
 
-            card.addEventListener('click', () => selectHero(metahuman.name, metahuman.powerstats, metahuman.images.sm));
+            card.addEventListener('click', () => selectHero(card, metahuman.name, metahuman.powerstats, metahuman.images.sm));
 
             main.appendChild(card);
         });
     }
 
-    function selectHero(heroName, heroStats, heroImage) {
+    function selectHero(card, heroName, heroStats, heroImage) {
         const hero1 = document.querySelector('.hero1');
         const hero2 = document.querySelector('.hero2');
+
+        // Remove 'selected' class from previously selected cards
+        document.querySelectorAll('.card').forEach(card => {
+            card.classList.remove('selected');
+        });
+
+        // Add 'selected' class to the clicked card
+        card.classList.add('selected');
 
         selectedHeroes.push({ name: heroName, stats: heroStats, image: heroImage });
 
         if (hero1.textContent === 'Select One Hero') {
             hero1.textContent = heroName;
-        } 
-        else if (hero2.textContent === 'Select One Hero') {
+        } else if (hero2.textContent === 'Select One Hero') {
             hero2.textContent = heroName;
         }
 
@@ -79,22 +88,20 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-
-
     function displayWinner() {
         const hero1 = selectedHeroes[0];
         const hero2 = selectedHeroes[1];
-    
+
         const hero1Stats = hero1.stats;
         const hero2Stats = hero2.stats;
-    
+
         const hero1Total = hero1Stats.intelligence + hero1Stats.strength + hero1Stats.speed +
-                           hero1Stats.durability + hero1Stats.power + hero1Stats.combat;
+            hero1Stats.durability + hero1Stats.power + hero1Stats.combat;
         const hero2Total = hero2Stats.intelligence + hero2Stats.strength + hero2Stats.speed +
-                           hero2Stats.durability + hero2Stats.power + hero2Stats.combat;
-    
+            hero2Stats.durability + hero2Stats.power + hero2Stats.combat;
+
         let winnerMessage = '';
-        
+
         if (hero1Total > hero2Total) {
             winnerMessage = `${hero1.name} 🏆 WINNER! 🏆`;
         } else if (hero2Total > hero1Total) {
@@ -102,21 +109,21 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             winnerMessage = `It's a draw!`;
         }
-    
+
         document.getElementById('winner').textContent = winnerMessage;
         document.getElementById('result').textContent = `${hero1.name}: ${hero1Total} vs ${hero2.name}: ${hero2Total}`;
-    
+
         document.getElementById('hero1Image').src = hero1.image;
         document.getElementById('hero1Name').textContent = hero1.name;
-    
+
         document.getElementById('hero2Image').src = hero2.image;
         document.getElementById('hero2Name').textContent = hero2.name;
-    
+
         const hero1StatsContainer = document.getElementById('hero1Stats');
         hero1StatsContainer.innerHTML = '';
         const hero2StatsContainer = document.getElementById('hero2Stats');
         hero2StatsContainer.innerHTML = '';
-    
+
         const stats = [
             { name: 'Intelligence', value1: hero1Stats.intelligence, value2: hero2Stats.intelligence },
             { name: 'Strength', value1: hero1Stats.strength, value2: hero2Stats.strength },
@@ -125,27 +132,25 @@ document.addEventListener('DOMContentLoaded', function () {
             { name: 'Power', value1: hero1Stats.power, value2: hero2Stats.power },
             { name: 'Combat', value1: hero1Stats.combat, value2: hero2Stats.combat }
         ];
-    
+
         stats.forEach(stat => {
             const statElement1 = document.createElement('p');
-            statElement1.textContent = `${stat.value1}`;
+            statElement1.textContent = `${stat.value1} ${stat.name}`;
             hero1StatsContainer.appendChild(statElement1);
-    
+
             const statElement2 = document.createElement('p');
-            statElement2.textContent = `${stat.value2}`;
+            statElement2.textContent = `${stat.value2} ${stat.name}`;
             hero2StatsContainer.appendChild(statElement2);
         });
-    
+
         document.getElementById('winnerModal').style.display = 'block';
-        
+
         selectedHeroes = [];
     }
 
-    document.querySelector('.close-button').addEventListener('click', function() {
+    document.querySelector('.close-button').addEventListener('click', function () {
         document.getElementById('winnerModal').style.display = 'none';
     });
-
-    
 
     function createPagination() {
         const pagination = document.querySelector('.pagination');
@@ -176,6 +181,17 @@ document.addEventListener('DOMContentLoaded', function () {
         pagination.appendChild(prevButton);
         pagination.appendChild(nextButton);
     }
+
+    function filterHeroes() {
+        const searchInput = document.querySelector('.search').value.toLowerCase();
+        filteredData = data.filter(metahuman => metahuman.name.toLowerCase().includes(searchInput));
+        totalPages = Math.ceil(filteredData.length / itemsPerPage);
+        currentPage = 1; // Reset to the first page
+        displayPage(currentPage);
+        createPagination();
+    }
+
+    document.querySelector('.search').addEventListener('input', filterHeroes);
 
     fetchMetahumanData();
 });
